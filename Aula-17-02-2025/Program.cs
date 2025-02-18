78a29df8ec13b17dc03c3bd2945dc7fd5ffc7df4
robinsoncruz <robinsoncruz@UNI-LAB2-PROF>
2025-02-18 19:23:24 -0300

    Adicionar arquivos de projeto.

---
+++ Aula-17-02-2025/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+double x1, x2, y1, y2,d,p1,p2;
+Console.Write("Digite x1:");
+x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite x2:");
+x2 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite y1:");
+y1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite y2:");
+y2 = double.Parse(Console.ReadLine());
+
+p1 = Math.Pow(x1 - x2, 2);
+p2 = Math.Pow(y1 - y2, 2);
+
+d = Math.Sqrt(p1 + p2);
+
+Console.Write(d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
