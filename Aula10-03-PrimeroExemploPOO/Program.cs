00c4510799282595e383002857bb282690498a74
robinsoncruz <robinsoncruz@UNI-LAB2-PROF>
2025-03-11 20:08:58 -0300

    Primeiro Exemplo de POO

---
+++ Aula10-03-PrimeroExemploPOO/Program.cs
@@ -0,0 +1,40 @@
+﻿//Instancia New
+
+Cliente _cli;
+_cli = new Cliente();
+_cli.idade = 25;
+Console.WriteLine(_cli.idade);
+
+_cli.idade = 50;
+Console.WriteLine(_cli.idade);
+
+public class Cliente
+{
+    public string cpf;
+    public string nome;
+    public string status;
+    public int idade;
+    public void ImprirIdade()
+    {
+        Console.Write(idade + "AN");
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ Vetor/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[,] dados = new int[4,4];
+
+
+for(int linha = 0; linha <=3 ; linha++)
+{
+    for(int coluna = 0;coluna<=3;coluna++)
+    {
+        Console.Write("Digite o Valor:");
+        dados[linha,coluna] = int.Parse(Console.ReadLine());
+    }
+}
+
+
+
+
+
