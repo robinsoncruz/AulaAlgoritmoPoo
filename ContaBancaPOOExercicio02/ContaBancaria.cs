b4bcb1e0af1c48cf7647952b7cd6489aa80b6e26
robinsoncruz <robinsoncruz@UNI-LAB2-PROF>
2025-03-18 19:40:46 -0300

    Conta Bancaria

---
+++ ContaBancaPOOExercicio02/ContaBancaria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaPOOExercicio02
+{
+    internal class ContaBancaria
+    {
+        public string NumeroConta;
+        public string Titular;
+        public double Saldo=0;
+
+        public void Depositar(double valor)
+        {
+            Saldo = Saldo+ valor;
+        }
+
+    }
+}
---
+++ ContaBancaPOOExercicio02/Program.cs
@@ -0,0 +1,12 @@
+﻿using ContaBancaPOOExercicio02;
+
+ContaBancaria _contabancaria=new ContaBancaria();
+
+_contabancaria.Depositar(50);
+
+Console.Write(_contabancaria.Saldo);
+
+
+Console.ReadLine();
+
+
